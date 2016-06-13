e9548eccc46c5e6a2a59a8cfba0a14b07c1b6a18
tomtom <thomasdigregorio@me.com>
2016-06-13 14:07:41 +0200

    Ajout fichiers auto par IntelliJ

--- SolitaireChess/src/Fenetre.java
+++ SolitaireChess/src/Fenetre.java
@@ -6,15 +6,15 @@ import javax.swing.*;
 public class Fenetre extends JFrame
 {
 
-    public Fenetre()
-    {
-        setTitle("Fenêtre de test");
-        setSize(400, 400);
-        setLocationRelativeTo(null);
+	public Fenetre()
+	{
+		setTitle( "Fenêtre de test" );
+		setSize( 400, 400 );
+		setLocationRelativeTo( null );
 
-        add(new JButton("Bouton"));
+		add( new JButton( "Bouton" ) );
 
-        setVisible(true);
-    }
+		setVisible( true );
+	}
 
 }
